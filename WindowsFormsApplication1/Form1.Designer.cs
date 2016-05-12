8b62b325f9f68d78c2f0aeacbb3436fcb3dc06f0
RuanJG <RuanJG@git.com>
2016-05-12 09:00:59 +0800

    Ruan: base serial code

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startButton = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.logTextBox = new System.Windows.Forms.TextBox();
+            this.comSelectComboBox = new System.Windows.Forms.ComboBox();
+            this.rateComboBox = new System.Windows.Forms.ComboBox();
+            this.sendTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(350, 17);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(40, 24);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(78, 16);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // logTextBox
+            // 
+            this.logTextBox.Location = new System.Drawing.Point(40, 99);
+            this.logTextBox.Multiline = true;
+            this.logTextBox.Name = "logTextBox";
+            this.logTextBox.ReadOnly = true;
+            this.logTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.logTextBox.Size = new System.Drawing.Size(385, 224);
+            this.logTextBox.TabIndex = 2;
+            this.logTextBox.TextChanged += new System.EventHandler(this.logTextBox_TextChanged);
+            // 
+            // comSelectComboBox
+            // 
+            this.comSelectComboBox.FormattingEnabled = true;
+            this.comSelectComboBox.Location = new System.Drawing.Point(40, 60);
+            this.comSelectComboBox.Name = "comSelectComboBox";
+            this.comSelectComboBox.Size = new System.Drawing.Size(78, 20);
+            this.comSelectComboBox.TabIndex = 3;
+            this.comSelectComboBox.SelectedIndexChanged += new System.EventHandler(this.comSelectComboBox_SelectedIndexChanged);
+            // 
+            // rateComboBox
+            // 
+            this.rateComboBox.FormattingEnabled = true;
+            this.rateComboBox.Location = new System.Drawing.Point(141, 60);
+            this.rateComboBox.Name = "rateComboBox";
+            this.rateComboBox.Size = new System.Drawing.Size(121, 20);
+            this.rateComboBox.TabIndex = 4;
+            this.rateComboBox.SelectedIndexChanged += new System.EventHandler(this.rateComboBox_SelectedIndexChanged);
+            // 
+            // sendTextBox
+            // 
+            this.sendTextBox.Location = new System.Drawing.Point(40, 352);
+            this.sendTextBox.Name = "sendTextBox";
+            this.sendTextBox.Size = new System.Drawing.Size(385, 21);
+            this.sendTextBox.TabIndex = 5;
+            this.sendTextBox.TextChanged += new System.EventHandler(this.sendTextBox_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 330);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "send text :";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(456, 410);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.sendTextBox);
+            this.Controls.Add(this.rateComboBox);
+            this.Controls.Add(this.comSelectComboBox);
+            this.Controls.Add(this.logTextBox);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.startButton);
+            this.Name = "mainForm";
+            this.Text = "mainForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox logTextBox;
+        private System.Windows.Forms.ComboBox comSelectComboBox;
+        private System.Windows.Forms.ComboBox rateComboBox;
+        private System.Windows.Forms.TextBox sendTextBox;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Threading;//用于启用线程类；
+using System.IO.Ports;//用于调用串口类函数
+
+
+/*
+public string iPort = "com1"; //默认为串口1
+public int iRate = 9600; //波特率1200,2400,4800,9600
+public byte bSize = 8; //8 bits
+public int iTimeout = 1000; //延时时长
+public SerialPort serialPort1 = new SerialPort();//定义一个串口类的串口变量
+string serialReadString; //用于串口接收数据
+public bool IsCirlce;//判断是否选用循环发送数据
+public Thread Thd_Send; //开辟一个专用于发送数据的线程        
+public byte[] recb;  //用于存放接收数据的数组
+*/
+
+
+
+namespace WindowsFormsApplication1
+{
+    public partial class mainForm : Form
+    {
+
+        int count=0;
+        SerialPort serial = new SerialPort();
+        Thread com_read_thread ;
+
+        private delegate void DelegateComRead();
+
+        public mainForm()
+        {
+            InitializeComponent();
+            comRate_Init();
+            com_Init();
+        }
+
+        private void comRate_Init()
+        {
+            rateComboBox.Items.Add("115200");
+            rateComboBox.Items.Add("100000");
+            rateComboBox.Items.Add("57600");
+            rateComboBox.Items.Add("9600");
+            rateComboBox.SelectedIndex = 0;
+        }
+        private void com_Init()
+        {
+            //comSelectComboBox.Items.Add("NONE");
+            comSelectComboBox.Items.Clear();
+            com_Load();
+            comSelectComboBox.SelectedIndex = 0;
+
+            serial.DataBits = 8;
+            serial.StopBits = StopBits.One;
+            serial.Parity = Parity.None;
+            serial.ReadTimeout = 1000;//500ms
+       
+        }
+        private void com_Load()
+        {
+            string[] ports = SerialPort.GetPortNames();
+            foreach (string s in ports)
+            {
+                //cmb_port.Items.Add(s);
+                comSelectComboBox.Items.Add(s);
+            }
+        }
+        private void log(string str)
+        {
+            logTextBox.AppendText(str);
+        }
+        private void ComRead()
+        {
+            ////第三种发送接收方式
+            //byte[] SendBuf = new byte[256];
+            //SendBuf = System.Text.Encoding.UTF8.GetBytes(txtSend.Text);
+            //int len = txtSend.Text.Length;
+            //sp.Write(SendBuf, 0, len);
+
+            //byte[] RecieveBuf = new byte[256];
+            //sp.Read(RecieveBuf, 0, 256);
+            //string strRecieve3 = System.Text.Encoding.UTF8.GetString(RecieveBuf);
+            //txtRecieve.Text = strRecieve3;
+            while (true)
+            {
+                Thread.Sleep(500);
+                logTextBox.AppendText("sdf");
+            }
+
+        }
+        private  void thread_com_read()
+        {
+            byte[] RecieveBuf = new byte[256];
+            int len;
+
+            while (true)
+            {
+                Thread.Sleep(10);
+                Array.Clear(RecieveBuf,0,RecieveBuf.Length);
+                try
+                {
+                    len = serial.Read(RecieveBuf, 0, RecieveBuf.Length-1);
+                    logTextBox.BeginInvoke(new MethodInvoker(delegate { log(System.Text.Encoding.UTF8.GetString(RecieveBuf)); }));
+                }
+                catch
+                {
+                    logTextBox.BeginInvoke(new MethodInvoker(delegate { log("get a catch\n"); }));
+                }
+            }
+        }
+        
+        //##############################################################  UI fucntion
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            if (serial.IsOpen){
+                serial.Close();
+                com_read_thread.Abort();
+                startButton.Text = "Start";
+                log("serial Close\n");
+            }else{
+                serial.Open();
+                com_read_thread = new Thread(new ThreadStart(thread_com_read));
+                com_read_thread.IsBackground = true;
+                com_read_thread.Start();
+                startButton.Text = "Stop";
+                log("serial Starting\n");
+            }
+            
+        }
+        private void comSelectComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            serial.PortName = comSelectComboBox.Items[comSelectComboBox.SelectedIndex].ToString();
+            log("com select "+serial.PortName+","+serial.BaudRate+"\n");
+           
+        }
+        private void rateComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            serial.BaudRate = int.Parse(rateComboBox.Items[rateComboBox.SelectedIndex].ToString());
+            log("com select " + serial.PortName + "," + serial.BaudRate + "\n");
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void logTextBox_TextChanged(object sender, EventArgs e)
+        {
+            //log("log text changed\n");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sendTextBox_TextChanged(object sender, EventArgs e)
+        {
+            string cmd;
+            if (sendTextBox.TextLength > 1 && sendTextBox.Text[sendTextBox.TextLength - 2].Equals('\\') && sendTextBox.Text[sendTextBox.TextLength - 1].Equals('n'))
+            {
+                cmd = sendTextBox.Text;
+                sendTextBox.Clear();
+                cmd = cmd.Remove(cmd.Length - 2);
+                if (serial.IsOpen)
+                {
+                    serial.Write(cmd);
+                    log("send cmd: " + cmd);
+                }
+            }
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
