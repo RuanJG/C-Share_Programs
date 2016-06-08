1c9d9478380ecb32416f35f599f43e5e73410eef
RuanJG <RuanJG@git.com>
2016-06-08 14:36:28 +0800

    Ruan: add DteFileDownloadApp Program

---
+++ DteFileDownloadApp/DteFileDownloadApp/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace DteFileDownloadApp
+{
+    partial class DteFileDownloadApp
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.downloadButton = new System.Windows.Forms.Button();
+            this.urlTextBox = new System.Windows.Forms.TextBox();
+            this.delButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.consoleTextBox = new System.Windows.Forms.RichTextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // downloadButton
+            // 
+            this.downloadButton.Location = new System.Drawing.Point(12, 372);
+            this.downloadButton.Name = "downloadButton";
+            this.downloadButton.Size = new System.Drawing.Size(83, 23);
+            this.downloadButton.TabIndex = 0;
+            this.downloadButton.Text = "下载";
+            this.downloadButton.UseVisualStyleBackColor = true;
+            this.downloadButton.Click += new System.EventHandler(this.downloadButton_Click);
+            // 
+            // urlTextBox
+            // 
+            this.urlTextBox.Location = new System.Drawing.Point(58, 109);
+            this.urlTextBox.Name = "urlTextBox";
+            this.urlTextBox.Size = new System.Drawing.Size(227, 21);
+            this.urlTextBox.TabIndex = 2;
+            this.urlTextBox.Text = "http://30.24.6.196:8080/";
+            this.urlTextBox.TextChanged += new System.EventHandler(this.urlTextBox_TextChanged);
+            // 
+            // delButton
+            // 
+            this.delButton.Location = new System.Drawing.Point(200, 372);
+            this.delButton.Name = "delButton";
+            this.delButton.Size = new System.Drawing.Size(87, 23);
+            this.delButton.TabIndex = 3;
+            this.delButton.Text = "清除远程文件";
+            this.delButton.UseVisualStyleBackColor = true;
+            this.delButton.Click += new System.EventHandler(this.delButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 112);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "地址：";
+            // 
+            // consoleTextBox
+            // 
+            this.consoleTextBox.Location = new System.Drawing.Point(12, 148);
+            this.consoleTextBox.Name = "consoleTextBox";
+            this.consoleTextBox.ReadOnly = true;
+            this.consoleTextBox.Size = new System.Drawing.Size(273, 202);
+            this.consoleTextBox.TabIndex = 5;
+            this.consoleTextBox.Text = "";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::DteFileDownloadApp.Properties.Resources.yunzhoulogo;
+            this.pictureBox1.Location = new System.Drawing.Point(49, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(199, 68);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // DteFileDownloadApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(305, 407);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.consoleTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.delButton);
+            this.Controls.Add(this.urlTextBox);
+            this.Controls.Add(this.downloadButton);
+            this.Name = "DteFileDownloadApp";
+            this.Text = "DteFileDownloadApp";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button downloadButton;
+        private System.Windows.Forms.TextBox urlTextBox;
+        private System.Windows.Forms.Button delButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox consoleTextBox;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ DteFileDownloadApp/DteFileDownloadApp/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Net;
+using System.Windows.Forms;
+
+namespace DteFileDownloadApp
+{
+    public partial class DteFileDownloadApp : Form
+    {
+        string reqURL = "http://30.24.6.196:8080/";
+        string fileName = "gps_dte.bin";
+
+
+        public DteFileDownloadApp()
+        {
+            InitializeComponent();
+            urlTextBox.Text = reqURL;
+            log("startup ok");
+        }
+
+        private void urlTextBox_TextChanged(object sender, EventArgs e)
+        {
+            reqURL = urlTextBox.Text;
+        }
+
+        private void downloadButton_Click(object sender, EventArgs e)
+        {
+            syncMainLoop();
+        }
+
+        private void delButton_Click(object sender, EventArgs e)
+        {
+            log("准备删除远程文件....");
+
+            WebClient client = new WebClient();
+            client.Headers.Clear();
+            client.Headers.Add("START", "" + -1);
+            byte[] res = client.DownloadData(reqURL);
+            if (res.Length > 0 && res[0] == 1)
+            {
+                log("远程文件已删除");
+            }
+            else
+            {
+                log("远程文件删除失败");
+            }
+        }
+
+
+
+        private void log(string str)
+        {
+            consoleTextBox.AppendText(str + "\r\n");
+        }
+        static int CheckDTE(byte[] data)
+        {
+            int dteSize = 0;
+            for (int idx = 0; idx < data.Length; ++idx)
+            {
+                if (data[idx] > 127)
+                {
+                    dteSize += (data[idx] & 127);
+                }
+            }
+            return dteSize;
+        }
+
+        private void syncMainLoop()
+        {
+            int retry = 10;
+            //del the old file
+            if (File.Exists(fileName))
+            {
+                File.WriteAllBytes(fileName, new byte[0]);
+            }
+
+            FileStream fs = new FileStream(fileName, FileMode.OpenOrCreate);
+            long fileLength = fs.Length;
+            fs.Seek(0, SeekOrigin.End);
+
+            while (0 < retry--)
+            {
+                try
+                {
+                    WebClient client = new WebClient();
+                    client.Headers.Add("START", "" + fileLength);
+                    byte[] data = client.DownloadData(reqURL);
+                    if (data.Length > 0)
+                    {
+                        fs.Write(data, 0, data.Length);
+                        fs.Flush();
+                        fileLength += data.Length;
+                        log("Sync " + data.Length + " bytes! DTE " + CheckDTE(data) + " bytes!");
+                        log("已保存到 " + fileName);
+                    }
+                    else
+                    {
+                        //Thread.Sleep(500);
+                        log("retry "+retry);
+                    }
+                }
+                catch (Exception e)
+                {
+                    log("Download error:" + e.ToString());
+                    //Thread.Sleep(500);
+                }
+
+            }
+            fs.Close();
+
+            log("完成！！");
+        }
+
+    }
+}
